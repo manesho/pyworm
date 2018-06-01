8f173b586fe715e09fecbe4e1a4ac538243f5508
Manes Hornung ITP <hornung@itp.unibe.ch>
2018-06-01 11:05:37 +0200

    rule generator for mu=0 transitions

--- rulegenerator.py
+++ rulegenerator.py
@@ -12,3 +12,9 @@ def generate_decayrules(mu3=0,mu8=0):
 		rules = { wtype:mu0rules for wtype in ["u>d","d>u", "u>s","s>u","d>s","s>d"] }
 		return rules
 
+def generate_2f_transitio_nrules(mu3=0,mu8=0):
+		mu0rules = {"FWD":{"=":1, "x":0},
+					"BWD":{"=":1, "x":0}}
+		rules = { wtype:mu0rules for wtype in ["u>d","d>u", "u>s","s>u","d>s","s>d"] }
+		return rules
+
